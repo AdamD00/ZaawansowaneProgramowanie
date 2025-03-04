8bdb954b74d24f1bb832e41b0bc2b1324042c923
Mrokon <56162325+AdamD00@users.noreply.github.com>
2025-03-04 12:24:24 +0100

    Update zadanie6

--- zadanie6.py
+++ zadanie6.py
@@ -1,1 +1,19 @@
 #Rozmiary okien
+import cv2
+
+#Wczytanie obrazu
+image = cv2.imread("image.jpg")
+
+
+if image is None:
+    print("Błąd: nie można odczytać")
+else:
+    print("Obraz wczytano")
+
+cv2.namedWindow("Kotek", cv2.WINDOW_NORMAL)
+
+cv2.imshow("Kotek",image) # tworzenie okna
+cv2.waitKey(0) # czeka na przycisk - okno się nie zamyka
+cv2.destroyAllWindow() # Zamyka okna
+
+
